09cb9518615f47b21b7dfa0706e749ad29ec0365
gioimtg2003 <conggioi.pro264@gmail.com>
2023-06-02 23:50:31 +0700

    update docker

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/configs/config.php';
 require_once __DIR__ . '/app/controllers/baseController.php';
-ob_start();
 require_once __DIR__ . '/inc/header.php';
 require_once __DIR__ . '/app/models/connectDB.php';
 require_once __DIR__ . '/app/App.php';
